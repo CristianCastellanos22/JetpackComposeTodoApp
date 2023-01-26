cfb56db4f48ccfcb062e25b1d34bf91183f50506
ccastellanostribal <ccastellanos@tribalworldwide.gt>
2023-01-26 14:12:50 -0500

    Add dependencies Livedata, created mutablelivdata and livedata to managed the states, created dialog to add task.

--- app/src/main/java/com/cristian/jetpackcomposetodoapp/todoapp/ui/TasksScreen.kt
+++ app/src/main/java/com/cristian/jetpackcomposetodoapp/todoapp/ui/TasksScreen.kt
@@ -1,8 +1,73 @@
 package com.cristian.jetpackcomposetodoapp.todoapp.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 
 @Composable
 fun TasksScreen(tasksViewModel: TasksViewModel) {
 
+    val showDialog: Boolean by tasksViewModel.showDialog.observeAsState(false)
+    Box(modifier = Modifier.fillMaxSize()) {
+        AddTasksDialog(showDialog, onDismiss = { tasksViewModel.onDialogClosed() }, onTaskAdded = { tasksViewModel.onTaskCreated(it) })
+        FabDialog(Modifier.align(Alignment.BottomEnd), tasksViewModel)
+    }
+
+}
+
+@Composable
+fun FabDialog(modifier: Modifier, tasksViewModel: TasksViewModel) {
+    FloatingActionButton(onClick = {
+        tasksViewModel.onShowDialogClick()
+    }, modifier = modifier) {
+        Icon(Icons.Filled.Add, contentDescription = "")
+    }
+}
+
+@Composable
+fun AddTasksDialog(show: Boolean, onDismiss: () -> Unit, onTaskAdded: (String) -> Unit) {
+    var myTask by remember { mutableStateOf("") }
+    if (show) {
+        Dialog(onDismissRequest = { onDismiss() }) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.White)
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = "Añade una nueva tarea",
+                    fontSize = 16.sp,
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(modifier = Modifier.size(16.dp))
+                TextField(
+                    value = myTask,
+                    onValueChange = { myTask = it },
+                    singleLine = true,
+                    maxLines = 1
+                )
+                Spacer(modifier = Modifier.size(16.dp))
+                Button(onClick = { onTaskAdded(myTask) }, modifier = Modifier.fillMaxWidth()) {
+                    Text(text = "Añadir tarea")
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/cristian/jetpackcomposetodoapp/todoapp/ui/TasksViewModel.kt
+++ app/src/main/java/com/cristian/jetpackcomposetodoapp/todoapp/ui/TasksViewModel.kt
@@ -1,9 +1,27 @@
 package com.cristian.jetpackcomposetodoapp.todoapp.ui
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class TasksViewModel @Inject constructor(): ViewModel() {
+    private val _showDialog = MutableLiveData<Boolean>()
+    val showDialog: LiveData<Boolean> = _showDialog
+
+    fun onDialogClosed() {
+        _showDialog.value = false
+    }
+
+    fun onTaskCreated(task: String) {
+        _showDialog.value = false
+        Log.i("Task created", task)
+    }
+
+    fun onShowDialogClick() {
+        _showDialog.value = true
+    }
 }
