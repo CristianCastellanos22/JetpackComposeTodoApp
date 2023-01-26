14c4066dc96fc4ad6a860617302786ad8288c510
ccastellanostribal <ccastellanos@tribalworldwide.gt>
2023-01-26 15:45:32 -0500

    Created model to managed task, add and remove task, created lazycolumn to show task.

--- app/src/main/java/com/cristian/jetpackcomposetodoapp/todoapp/ui/TasksScreen.kt
+++ app/src/main/java/com/cristian/jetpackcomposetodoapp/todoapp/ui/TasksScreen.kt
@@ -1,31 +1,40 @@
 package com.cristian.jetpackcomposetodoapp.todoapp.ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
+import com.cristian.jetpackcomposetodoapp.todoapp.ui.model.TaskModel
 
 @Composable
 fun TasksScreen(tasksViewModel: TasksViewModel) {
 
     val showDialog: Boolean by tasksViewModel.showDialog.observeAsState(false)
     Box(modifier = Modifier.fillMaxSize()) {
-        AddTasksDialog(showDialog, onDismiss = { tasksViewModel.onDialogClosed() }, onTaskAdded = { tasksViewModel.onTaskCreated(it) })
+        AddTasksDialog(
+            showDialog,
+            onDismiss = { tasksViewModel.onDialogClosed() },
+            onTaskAdded = { tasksViewModel.onTaskCreated(it) })
         FabDialog(Modifier.align(Alignment.BottomEnd), tasksViewModel)
+        TasksList(tasksViewModel)
     }
 
 }
@@ -64,10 +73,51 @@ fun AddTasksDialog(show: Boolean, onDismiss: () -> Unit, onTaskAdded: (String) -
                     maxLines = 1
                 )
                 Spacer(modifier = Modifier.size(16.dp))
-                Button(onClick = { onTaskAdded(myTask) }, modifier = Modifier.fillMaxWidth()) {
+                Button(onClick = {
+                    onTaskAdded(myTask)
+                    myTask = ""
+                }, modifier = Modifier.fillMaxWidth()) {
                     Text(text = "Añadir tarea")
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun TasksList(tasksViewModel: TasksViewModel) {
+    val myTask: List<TaskModel> = tasksViewModel.tasks
+    LazyColumn {
+        items(myTask, key = { it.id }) { task ->
+            ItemTask(task, tasksViewModel)
+        }
+    }
+}
+
+@Composable
+fun ItemTask(taskModel: TaskModel, tasksViewModel: TasksViewModel) {
+    Card(
+        Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+            .pointerInput(Unit) {
+                detectTapGestures(
+                    onLongPress = {
+                        tasksViewModel.onItemRemove(taskModel)
+                    }
+                )
+
+            }, elevation = 8.dp
+    ) {
+        Row(Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                text = taskModel.task, modifier = Modifier
+                    .padding(horizontal = 4.dp)
+                    .weight(1f)
+            )
+            Checkbox(
+                checked = taskModel.selected,
+                onCheckedChange = { tasksViewModel.onCheckBoxSelected(taskModel) })
         }
     }
 }
--- app/src/main/java/com/cristian/jetpackcomposetodoapp/todoapp/ui/TasksViewModel.kt
+++ app/src/main/java/com/cristian/jetpackcomposetodoapp/todoapp/ui/TasksViewModel.kt
@@ -1,16 +1,21 @@
 package com.cristian.jetpackcomposetodoapp.todoapp.ui
 
 import android.util.Log
+import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.cristian.jetpackcomposetodoapp.todoapp.ui.model.TaskModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class TasksViewModel @Inject constructor(): ViewModel() {
+class TasksViewModel @Inject constructor() : ViewModel() {
     private val _showDialog = MutableLiveData<Boolean>()
     val showDialog: LiveData<Boolean> = _showDialog
+
+    private val _tasks = mutableStateListOf<TaskModel>()
+    val tasks: List<TaskModel> = _tasks
 
     fun onDialogClosed() {
         _showDialog.value = false
@@ -18,10 +23,20 @@ class TasksViewModel @Inject constructor(): ViewModel() {
 
     fun onTaskCreated(task: String) {
         _showDialog.value = false
-        Log.i("Task created", task)
+        _tasks.add(TaskModel(task = task))
     }
 
     fun onShowDialogClick() {
         _showDialog.value = true
+    }
+
+    fun onCheckBoxSelected(taskModel: TaskModel) {
+        val index = _tasks.indexOf(taskModel)
+        _tasks[index] = taskModel.copy(selected = !taskModel.selected)
+    }
+
+    fun onItemRemove(taskModel: TaskModel) {
+        val task = _tasks.find { it.id == taskModel.id }
+        _tasks.remove(task)
     }
 }
