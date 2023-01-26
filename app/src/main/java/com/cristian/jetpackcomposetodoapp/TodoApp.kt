b904a81db3ba2ae99eef30ca0b3161b61446a227
ccastellanostribal <ccastellanos@tribalworldwide.gt>
2023-01-26 12:24:59 -0500

    Initial commit

---
+++ app/src/main/java/com/cristian/jetpackcomposetodoapp/TodoApp.kt
@@ -0,0 +1,5 @@
+package com.cristian.jetpackcomposetodoapp
+
+import android.app.Application
+
+class TodoApp(): Application()
---
+++ app/src/main/java/com/cristian/jetpackcomposetodoapp/todoapp/ui/TasksViedModel.kt
@@ -0,0 +1,6 @@
+package com.cristian.jetpackcomposetodoapp.todoapp.ui
+
+import androidx.lifecycle.ViewModel
+
+class TasksViedModel: ViewModel() {
+}
